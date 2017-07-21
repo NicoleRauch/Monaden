53a02c5817703e738cbb7dc70a152691172158ef
Nicole Rauch <nicole.m@gmx.de>
2017-07-21 16:40:52 +0200

    Update Haskell representation

--- haskell/Exception.hs
+++ haskell/Exception.hs
@@ -1,8 +1,19 @@
+import Control.Applicative -- Otherwise you can't do the Applicative instance.
+import Control.Monad (liftM, ap)
+
 data Exceptional e a =
      Success a
    | Exception e
    deriving (Show)
- 
+
+-- as per the Functor-Applicative-Monad Proposal: (https://wiki.haskell.org/Functor-Applicative-Monad_Proposal)
+instance Functor (Exceptional e) where
+  fmap = liftM
+
+instance Applicative (Exceptional e) where
+  pure  = return
+  (<*>) = ap
+
 instance Monad (Exceptional e) where
    return              =  Success
    Exception l >>= _   =  Exception l
@@ -17,19 +28,17 @@ catch (Success r)    _ = Success r
 
 
 
-data Exceptions = RuntimeException String
+data Exceptions = IllegalArgumentException String
+  deriving (Show)
 
 throwSomething x
-   | x == 7    = throw (RuntimeException "Ich hasse 7!")
+   | x == 7    = throw (IllegalArgumentException "Ich hasse 7!")
    | otherwise = Success (x + 1)
 
 mal3 :: Int -> Int
 mal3 x = 3 * x
 
-resultOf (Exception (RuntimeException s)) = "Error: " ++ s
-resultOf (Success r) = "Result: " ++ (show r)
-
 main = do {
-   putStrLn (resultOf $ (throwSomething 7) >>= (return . mal3));
-   putStrLn (resultOf $ (throwSomething 12) >>= (return . mal3))
+   print $ (throwSomething 7) >>= (return . mal3);
+   print $ (throwSomething 12) >>= (return . mal3)
 }
