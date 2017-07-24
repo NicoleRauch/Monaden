675039b64c479f68dfd7d9f4e1d9695f7b15b993
Nicole Rauch <nicole.m@gmx.de>
2017-07-24 09:07:04 +0200

    Switching to "show" and "print".

--- haskell/Sysout.hs
+++ haskell/Sysout.hs
@@ -1,5 +1,6 @@
 
 data Sysout a = ResOut a String
+     deriving (Show)
 
 (ResOut res1 out1) >>= f = let (ResOut res2 out2) = f res1
                            in (ResOut res2 (out1 ++ " " ++ out2))
@@ -11,10 +12,6 @@ numberAndText = ResOut 7 "Hallo Herbstcampus!"
 
 calcAndText x = ResOut (4 + x) "Ich kann rechnen!"
 
-resultOf (ResOut res out) = show res
-outputOf (ResOut res out) = out
-
 main = do {
-     putStrLn (resultOf (numberAndText Main.>>= calcAndText));
-     putStrLn (outputOf (numberAndText Main.>>= calcAndText))
+     print (numberAndText Main.>>= calcAndText);
 }
